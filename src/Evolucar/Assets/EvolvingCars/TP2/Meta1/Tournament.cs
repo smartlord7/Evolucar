058ff5d4763bc3ce5715530c5ff6fa6150424157
Razoor <tiagofilipe122@gmail.com>
2022-04-13 04:40:09 +0100

    MASTER:DEVELOP TOURNY SELECTION MECHANISM FUNCTION.

--- src/Evolucar/Assets/EvolvingCars/TP2/Meta1/Tournament.cs
+++ src/Evolucar/Assets/EvolvingCars/TP2/Meta1/Tournament.cs
@@ -32,7 +32,24 @@ public class Tournament : SelectionBase
         IList<CarChromosome> population = generation.Chromosomes.Cast<CarChromosome>().ToList(); // Current Population: We will select individuals from here 
         IList<IChromosome> parents = new List<IChromosome>(); //List that will return the individuals that will mate, i.e. that will undergo variation
 
-        //YOUR CODE HERE
+        int i;
+        int k;
+        for (i = 0; i < number; i++)
+        {
+            int[] randomIndexes = RandomizationProvider.Current.GetUniqueInts(Size, 0, population.Count);
+            IChromosome winner = null;
+            float winnerFitness = -1;
+            for (k = 0; k < Size; k++)
+            {
+                int individualIndex = randomIndexes[k];
+                if (winner.Equals(null) || population[individualIndex].Fitness > winnerFitness)
+                {
+                    winner = population[individualIndex];
+                    winnerFitness = population[individualIndex].Fitness;
+                }
+            }
+            parents.Add(winner);
+        }
 
         return parents;
     }
