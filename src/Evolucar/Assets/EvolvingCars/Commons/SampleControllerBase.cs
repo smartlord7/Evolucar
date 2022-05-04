eafed5fe5057ddf6ee6b5d031adec61c441675fa
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-05-04 22:44:49 +0100

    MASTER: Improve log code.

--- src/Evolucar/Assets/EvolvingCars/Commons/SampleControllerBase.cs
+++ src/Evolucar/Assets/EvolvingCars/Commons/SampleControllerBase.cs
@@ -1,16 +1,24 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using GeneticSharp.Domain;
-using System.Threading;
-using System;
-using System.Linq;
-using UnityEngine.SceneManagement;
+﻿using System;
 using System.IO;
-using GeneticSharp.Domain.Chromosomes;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Linq;
+using System.Threading;
+using GeneticSharp.Domain;
 using GeneticSharp.Runner.UnityApp.Car;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public abstract class SampleControllerBase : MonoBehaviour {
+public abstract class SampleControllerBase : MonoBehaviour
+{
+    #region Const
+
+    const string DELIMITER_OUTPUT = ";";
+    const string NAME_FILE_EVOLUTION = "EvolutionLog.csv";
+
+    #endregion Const
+
+    #region Private Properties
+
 
     private Thread m_gaThread;
     private Text m_generationText;
@@ -20,15 +28,29 @@ public abstract class SampleControllerBase : MonoBehaviour {
     private double m_previousBestFitness;
     private double m_previousAverageFitness;
     private string folderName;
-    private string newFileName = "EvolutionLog.csv";
+
+    #endregion Private Properties
+
+    #region Public Properties
 
     protected GeneticAlgorithm GA { get; private set; }
     protected bool ChromosomesCleanupEnabled { get; set; }
     protected bool ShowPreviousInfoEnabled { get; set; } = true;
+
+    #endregion Public Properties
+
+
+    #region Public Properties
+
     public Rect Area { get; private set; }
 
-	private void Start()
-	{
+    #endregion Public Properties
+
+
+    #region Private Methods
+
+    private void Start()
+    {
         Application.runInBackground = true;
         var sampleArea = GameObject.Find("SampleArea");
         Area = sampleArea == null
@@ -58,7 +80,8 @@ public abstract class SampleControllerBase : MonoBehaviour {
         }
 
         GA = CreateGA();
-        GA.GenerationRan += delegate {
+        GA.GenerationRan += delegate
+        {
             UpdateStatistics();
 
 
@@ -70,6 +93,7 @@ public abstract class SampleControllerBase : MonoBehaviour {
                 }
             }
         };
+
         StartSample();
 
         m_gaThread = new Thread(() =>
@@ -79,19 +103,20 @@ public abstract class SampleControllerBase : MonoBehaviour {
                 Thread.Sleep(1000);
                 GA.Start();
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                if(ex.Message.Length > 0)
+                if (ex.Message.Length > 0)
                 {
                     Debug.LogError($"GA thread error: {ex.Message}");
                 }
-                
+
             }
         });
-        m_gaThread.Start();
-	}
 
-    void Update()
+        m_gaThread.Start();
+    }
+
+    private void Update()
     {
         if (m_generationText != null && GA.Population.CurrentGeneration != null)
         {
@@ -102,7 +127,7 @@ public abstract class SampleControllerBase : MonoBehaviour {
                 m_generationText,
                 m_fitnessText,
                 GA.GenerationsNumber,
-                bestFitness, 
+                bestFitness,
                 averageFitness);
 
             if (ShowPreviousInfoEnabled && GA.GenerationsNumber > 1)
@@ -116,43 +141,21 @@ public abstract class SampleControllerBase : MonoBehaviour {
             }
         }
 
-
         UpdateSample();
     }
 
-	private void OnDestroy()
-	{
+    private void OnDestroy()
+    {
         GA.Stop();
         m_gaThread.Abort();
-        if(m_generationText != null)
+
+        if (m_generationText != null)
         {
             m_generationText.text = String.Empty;
             m_fitnessText.text = String.Empty;
 
             m_previousGenerationText.text = String.Empty;
             m_previousFitnessText.text = String.Empty;
-
-        }
-        
-	}
-
-	protected virtual void StartSample() 
-    {
-        
-    }
-
-    protected abstract GeneticAlgorithm CreateGA();
-
-    protected virtual void UpdateSample()
-    {
-        
-    }
-
-    protected void SetFitnessText(string text)
-    {
-        if(m_fitnessText != null)
-        {
-            m_fitnessText.text = text;
         }
     }
 
@@ -166,35 +169,29 @@ public abstract class SampleControllerBase : MonoBehaviour {
     {
         m_previousBestFitness = GA.BestChromosome.Fitness.Value;
         m_previousAverageFitness = GA.Population.CurrentGeneration.Chromosomes.Average(c => c.Fitness.Value);
-        //Debug.Log($"Generation: {GA.GenerationsNumber} - Best: ${m_previousBestFitness} - Average: ${m_previousAverageFitness}");
 
         var best = GA.BestChromosome as CarChromosome;
-        string pathToFile = this.folderName + "/" +this.newFileName;
-        string generationInfo = $"{GA.GenerationsNumber},{m_previousBestFitness},{m_previousAverageFitness},{best.MaxDistance},{best.MaxDistanceTime},{best.NumberOfWheels},{best.CarMass},{best.IsRoadComplete}" + Environment.NewLine;
+        string pathToFile = $"${this.folderName}/${NAME_FILE_EVOLUTION}";
+        string generationInfo = $"{GA.GenerationsNumber}${DELIMITER_OUTPUT}{m_previousBestFitness}${DELIMITER_OUTPUT}{m_previousAverageFitness}${DELIMITER_OUTPUT}{best.MaxDistance}${DELIMITER_OUTPUT}{best.MaxDistanceTime}${DELIMITER_OUTPUT}{best.NumberOfWheels}${DELIMITER_OUTPUT}{best.CarMass}${DELIMITER_OUTPUT}{best.IsRoadComplete}" + Environment.NewLine;
 
 
         if (!File.Exists(pathToFile))
         {
-            string header = "Generation,BestFitness,AverageFitnessPopulation,BestMaxDistance,BestMaxDistanceTime,BestNumberOfWheels,BestCarMass,BestIsRoadComplete" + Environment.NewLine;
+            string header = $"Generation${DELIMITER_OUTPUT}BestFitness${DELIMITER_OUTPUT}AverageFitnessPopulation${DELIMITER_OUTPUT}BestMaxDistance${DELIMITER_OUTPUT}BestMaxDistanceTime${DELIMITER_OUTPUT}BestNumberOfWheels${DELIMITER_OUTPUT}BestCarMass${DELIMITER_OUTPUT}BestIsRoadComplete" + Environment.NewLine;
             Debug.Log(header);
 
             File.WriteAllText(pathToFile, header);
         }
+
         Debug.Log(generationInfo);
         File.AppendAllText(pathToFile, generationInfo);
         DumpOverallBest();
-        
-
-
-
     }
 
     private void DumpOverallBest()
     {
-        //Debug.Log(GA.BestChromosome);
         string pathToFile = this.folderName + "/OverallBestGenotype.txt";
         File.WriteAllText(pathToFile, GA.BestChromosome.ToString());
-        
     }
 
     private void CreateStatsFolder()
@@ -202,6 +199,31 @@ public abstract class SampleControllerBase : MonoBehaviour {
         string sceneName = SceneManager.GetActiveScene().name;
         string configName = "-crossProb-" + GeneticAlgorithmConfigurations.crossoverProbability.ToString() + "-elite-" + GeneticAlgorithmConfigurations.eliteSize.ToString() + "-mutProb-" + GeneticAlgorithmConfigurations.mutationProbability.ToString() + "-tournamenteSize-" + GeneticAlgorithmConfigurations.tournamentSize.ToString();
         this.folderName = "Results/" + sceneName + "-" + DateTime.Now.ToString("yyyy-dd-M-HH-mm-ss") + configName;
-        System.IO.Directory.CreateDirectory(this.folderName);
+        Directory.CreateDirectory(this.folderName);
     }
+
+    #endregion Private Methods
+
+
+    #region Protected Methods
+
+    protected virtual void StartSample()
+    {
+    }
+
+    protected abstract GeneticAlgorithm CreateGA();
+
+    protected virtual void UpdateSample()
+    {
+    }
+
+    protected void SetFitnessText(string text)
+    {
+        if (m_fitnessText != null)
+        {
+            m_fitnessText.text = text;
+        }
+    }
+
+    #endregion Protected Methods
 }
