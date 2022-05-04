4f9f678e50732246a5322ff8f7a96fec4bec8d5b
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-05-04 21:51:44 +0100

    MASTER: Improve crossover script.

--- src/Evolucar/Assets/EvolvingCars/TP2/Meta1/SinglePointCrossover.cs
+++ src/Evolucar/Assets/EvolvingCars/TP2/Meta1/SinglePointCrossover.cs
@@ -1,29 +1,29 @@
-using GeneticSharp.Domain.Chromosomes;
-using System;
-using System.Linq;
-using UnityEngine;
-using GeneticSharp.Domain.Randomizations;
 using System.Collections.Generic;
+using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Randomizations;
 
 namespace GeneticSharp.Runner.UnityApp.Commons
 {
     public class SinglePointCrossover : ICrossover
     {
-  
-
-        
-
+        #region Public Properties
         public int ParentsNumber { get; private set; }
-
         public int ChildrenNumber { get; private set; }
-
         public int MinChromosomeLength { get; private set; }
-
         public bool IsOrdered { get; private set; } // indicating whether the operator is ordered (if can keep the chromosome order).
+
+        #endregion Public Properties
+
+
+        #region Protected Properties
 
         protected float crossoverProbability;
 
+        #endregion Protected Properties
+
+
+        #region Constructors
 
         public SinglePointCrossover(float crossoverProbability) : this(2, 2, 2, true)
         {
@@ -37,26 +37,35 @@ namespace GeneticSharp.Runner.UnityApp.Commons
             MinChromosomeLength = minChromosomeLength;
             IsOrdered = isOrdered;
         }
-        
+
+        #endregion Constructors
+
+
+        #region Public Methods
+
         public IList<IChromosome> Cross(IList<IChromosome> parents)
         {
-            IChromosome parent1 = parents[0];
-            IChromosome parent2 = parents[1];
-            IChromosome offspring1 = parent1.CreateNew();
-            IChromosome offspring2 = parent2.CreateNew();
             int i;
-            if (RandomizationProvider.Current.GetDouble()<= crossoverProbability)
+            var parent1 = parents[0];
+            var parent2 = parents[1];
+            var offspring1 = parent1.CreateNew();
+            var offspring2 = parent2.CreateNew();
+
+            if (RandomizationProvider.Current.GetDouble() <= crossoverProbability)
             {
-                int cutPoint = RandomizationProvider.Current.GetInt(1, parent1.Length);
+                var cutPoint = RandomizationProvider.Current.GetInt(1, parent1.Length);
+
                 for (i = 0; i < cutPoint; i++)
                 {
-                    offspring1.ReplaceGene(i,parent2.GetGene(i));
-                    offspring2.ReplaceGene(i,parent1.GetGene(i));
+                    offspring1.ReplaceGene(i, parent2.GetGene(i));
+                    offspring2.ReplaceGene(i, parent1.GetGene(i));
                 }
             }
-            
+
             return new List<IChromosome> { offspring1, offspring2 };
-            
+
         }
+
+        #endregion Public Methods
     }
 }
