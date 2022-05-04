5cc29ed2a9e47391c36966b01baf0894dde27e0b
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-05-04 21:44:41 +0100

    MASTER: Improve elitism script.

--- src/Evolucar/Assets/EvolvingCars/TP2/Meta1/Elitism.cs
+++ src/Evolucar/Assets/EvolvingCars/TP2/Meta1/Elitism.cs
@@ -1,30 +1,32 @@
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Populations;
 using GeneticSharp.Domain.Reinsertions;
 
-
 public class Elitism : ReinsertionBase
 {
+    #region Protected Properties
+
     protected int eliteSize = 0;
+
+    #endregion Protected Properties
+
+
+    #region Constructors
+
     public Elitism(int eliteSize) : base(false, false)
     {
         this.eliteSize = eliteSize;
     }
-    
-    protected override IList<IChromosome> PerformSelectChromosomes(IPopulation population, IList<IChromosome> offspring, IList<IChromosome> parents)
-    {
-       
-        var old_population = population.CurrentGeneration.Chromosomes.OrderByDescending(p => p.Fitness).ToList(); //previous population sorted by fitness
-        int i;
-        for (i = 0; i < eliteSize; i++)
-        {
-            offspring[i] = old_population[i];
-        }
 
-        return offspring;
-    }
-    
+    #endregion Constructors
+
+
+    #region Protected Methods
+
+    protected override IList<IChromosome> PerformSelectChromosomes(IPopulation population, IList<IChromosome> offspring, IList<IChromosome> parents)
+        => population.CurrentGeneration.Chromosomes.OrderByDescending(p => p.Fitness).Take(eliteSize) as IList<IChromosome>;
+
+    #endregion Protected Methods
 }
