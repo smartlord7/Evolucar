e1638499e3fc1370c0eb1981771ddfc5af3681ed
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-05-04 21:58:37 +0100

    MASTER: Improve mutation script.

--- src/Evolucar/Assets/EvolvingCars/TP2/Meta1/SinglePointMutation.cs
+++ src/Evolucar/Assets/EvolvingCars/TP2/Meta1/SinglePointMutation.cs
@@ -1,39 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Mutations;
 using GeneticSharp.Domain.Randomizations;
 
 public class SinglePointMutation : IMutation
 {
+    #region Public Properties
+
     public bool IsOrdered { get; private set; } // indicating whether the operator is ordered (if can keep the chromosome order).
+
+    #endregion Public Properties
+
+
+    #region Constructors
 
     public SinglePointMutation()
     {
         IsOrdered = true;
     }
 
+    #endregion Constructors
+
+
+    #region Public Methods
 
     public void Mutate(IChromosome chromosome, float probability)
     {
         int i;
+
         for (i = 0; i < chromosome.Length; i++)
         {
             if (RandomizationProvider.Current.GetDouble() <= probability)
             {
                 var geneValue = chromosome.GetGene(i).Value;
+
                 if (geneValue.Equals(1))
                 {
-                    chromosome.ReplaceGene(i,new Gene(0));
+                    chromosome.ReplaceGene(i, new Gene(0));
                 }
                 else
                 {
-                    chromosome.ReplaceGene(i,new Gene(1));
+                    chromosome.ReplaceGene(i, new Gene(1));
                 }
             }
         }
 
     }
 
+    #endregion Public Methods
 }
