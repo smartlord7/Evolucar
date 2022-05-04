af2ff2841cadbc6e92f857dec5358718c08c9f7f
Sancho Amaral Simões <ssimoes@ipn.pt>
2022-05-04 22:04:58 +0100

    MASTER: Improve tournament script.

--- src/Evolucar/Assets/EvolvingCars/TP2/Meta1/Tournament.cs
+++ src/Evolucar/Assets/EvolvingCars/TP2/Meta1/Tournament.cs
@@ -1,56 +1,73 @@
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Populations;
 using GeneticSharp.Domain.Randomizations;
 using GeneticSharp.Domain.Selections;
-using GeneticSharp.Infrastructure.Framework.Texts;
 using GeneticSharp.Runner.UnityApp.Car;
 
 public class Tournament : SelectionBase
 {
+    #region Const
+
+    const int DEFAULT_TOURNAMENT_SIZE = 2;
+
+    #endregion Const
+
+
+    #region Protected Properties
+
     protected int Size { get; set; }
-    public Tournament() : this(2)
+
+    #endregion Protected Properties
+
+
+    #region Constructors
+
+    public Tournament() : this(DEFAULT_TOURNAMENT_SIZE)
     {
     }
 
-
-    
-    public Tournament(int size) : base(2)
+    public Tournament(int size) : base(DEFAULT_TOURNAMENT_SIZE)
     {
         Size = size;
     }
 
-    
+    #endregion Constructors
 
 
+    #region Protected Methods
 
     protected override IList<IChromosome> PerformSelectChromosomes(int number, Generation generation)
     {
 
-        IList<CarChromosome> population = generation.Chromosomes.Cast<CarChromosome>().ToList(); // Current Population: We will select individuals from here 
-        IList<IChromosome> parents = new List<IChromosome>(); //List that will return the individuals that will mate, i.e. that will undergo variation
-        
         int i;
         int k;
+        var population = generation.Chromosomes.Cast<CarChromosome>().ToList(); // Current Population: We will select individuals from here 
+        var parents = new List<IChromosome>(); //List that will return the individuals that will mate, i.e. that will undergo variation
+
         for (i = 0; i < number; i++)
         {
-            int[] randomIndexes = RandomizationProvider.Current.GetUniqueInts(Size, 0, population.Count);
+            var randomIndexes = RandomizationProvider.Current.GetUniqueInts(Size, 0, population.Count);
+            var winnerFitness = -1.0f;
             IChromosome winner = null;
-            float winnerFitness = -1;
+
             for (k = 0; k < Size; k++)
             {
-                int individualIndex = randomIndexes[k];
+                var individualIndex = randomIndexes[k];
+
                 if (winner == null || population[individualIndex].Fitness > winnerFitness)
                 {
                     winner = population[individualIndex];
                     winnerFitness = population[individualIndex].Fitness;
                 }
             }
+
             parents.Add(winner);
         }
 
         return parents;
     }
+
+    #endregion Protected Methods
 }
